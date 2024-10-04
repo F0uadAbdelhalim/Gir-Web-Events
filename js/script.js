2ffc3d97d9353ec1a6968dfbef782478ac4b5a77
Mostafa Beder <mostafabder2@gmail.com>
2024-10-04 20:44:26 +0300

    Fix the splide progress

--- js/script.js
+++ js/script.js
@@ -82,19 +82,19 @@ try {
 
   splideServicesOne.mount();
 
-  var splide = new Splide(".progress", {
+  var splidePro = new Splide(".progress", {
     pagination: false,
   });
-  var bar = splide.root.querySelector(".my-slider-progress-bar");
+  var bar = splidePro.root.querySelector(".my-slider-progress-bar");
 
   // Updates the bar width whenever the carousel moves:
-  splide.on("mounted move", function () {
-    var end = splide.Components.Controller.getEnd() + 1;
-    var rate = Math.min((splide.index + 1) / end, 1);
+  splidePro.on("mounted move", function () {
+    var end = splidePro.Components.Controller.getEnd() + 1;
+    var rate = Math.min((splidePro.index + 1) / end, 1);
     bar.style.width = String(100 * rate) + "%";
   });
 
-  splide.mount();
+  splidePro.mount();
 
   var splide = new Splide(".hero__splide", {
     type: "loop",
